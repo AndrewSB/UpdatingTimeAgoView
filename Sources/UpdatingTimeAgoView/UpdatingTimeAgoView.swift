401a3bb1fdccdb98b1ecc2b8b14a5ac46d206bbf
Andrew Breckenridge <asbreckenridge@me.com>
2022-03-29 22:47:05 -0400

    implement UpdatingTimeAgoView

--- Sources/UpdatingTimeAgoView/UpdatingTimeAgoView.swift
+++ Sources/UpdatingTimeAgoView/UpdatingTimeAgoView.swift
@@ -1,6 +1,61 @@
-public struct UpdatingTimeAgoView {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import SwiftUI
+import Combine
 
-    public init() {
+public class UpdatingTimeAgoViewStore: ObservableObject {
+
+    @Published var currentTimeAgo: TimeInterval
+
+    private let targetDate: Date
+    private var cancellables = Set<AnyCancellable>()
+
+    public init(targetDate: Date, updateFrequency: TimeInterval) {
+        self.targetDate = targetDate
+        self.currentTimeAgo = Self.calculateTimeAgo(targetDate: targetDate)
+
+        Timer.publish(every: updateFrequency, tolerance: updateFrequency / 2, on: .main, in: .common)
+            .autoconnect()
+            .sink { [weak self] _ in
+                guard let self = self else { return }
+
+                self.currentTimeAgo = Self.calculateTimeAgo(targetDate: self.targetDate)
+            }
+            .store(in: &cancellables)
+    }
+
+    private static func calculateTimeAgo(targetDate: Date) -> TimeInterval {
+        return -targetDate.timeIntervalSinceNow
     }
 }
+
+public struct UpdatingTimeAgoView<Content: View>: View {
+
+    @ObservedObject var store: UpdatingTimeAgoViewStore
+
+    let formatter: (TimeInterval) -> String
+    let label: (String) -> Content
+
+    public init(targetDate: Date,
+         updateFrequency: TimeInterval = 5,
+         formatter: @escaping (TimeInterval) -> String = { "\($0)" },
+         @ViewBuilder label: @escaping (String) -> Content) {
+        self.store = UpdatingTimeAgoViewStore(targetDate: targetDate, updateFrequency: updateFrequency)
+        self.formatter = formatter
+        self.label = label
+    }
+
+
+    public var body: some View {
+        label(formatter(store.currentTimeAgo))
+    }
+}
+
+#if DEBUG
+struct UpdatingTimeAgoView_Previews: PreviewProvider {
+    static var previews: some View {
+        UpdatingTimeAgoView(targetDate: Date().addingTimeInterval(-20), updateFrequency: 1, label: { timeAgoString in
+            Text(timeAgoString)
+        })
+    }
+}
+#endif
